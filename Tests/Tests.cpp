5f28d242b2968ffa233f877b415c4824be8a4a35
Damian <dtrebilco@gmail.com>
2018-04-12 23:50:20 +1000

    Add random methods

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -58,9 +58,106 @@ inline uint32_t RandRange(uint32_t i_max)
   return XorShift32() % (i_max + 1); // i_max cannot be uint32 limit
 }
 
-
+///////////////////////////////////////////////////////////////////////////////
 template<class T, class P>
-bool RandomSelect(const std::vector<T>& items, P test, T& returnItem)
+bool RandomItemSortArray(std::vector<T>& items, P test, T& returnItem)
+{
+  // Put all the matching items at the end of the array
+  auto first = std::find_if(items.begin(), items.end(), test);
+  if (first != items.end())
+  {
+    for (auto i = first; ++i != items.end(); )
+    {
+      if (!test(*i))
+      {
+        std::swap(*first, *i);
+        *first++;
+      }
+    }
+  }
+    
+  auto itemCount = std::distance(first, items.end());
+  if (itemCount == 0)
+  {
+    return false;
+  }
+
+  // Select a random index
+  uint32_t matchIndex = RandRange((uint32_t)itemCount - 1);
+  returnItem = *(first + matchIndex);
+
+  return true;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+template<class T, class P>
+bool RandomItemTempArray(const std::vector<T>& items, P test, T& returnItem)
+{
+  std::vector<uint32_t> indices;
+  for (uint32_t i = 0; i < items.size(); i++)
+  {
+    // If matching the test
+    if (test(items[i]))
+    {
+      indices.push_back(i);
+    }
+  }
+
+  if (indices.size() == 0)
+  {
+    return false;
+  }
+
+  // Select a random index
+  uint32_t matchIndex = RandRange(indices.size() - 1);
+  returnItem = items[indices[matchIndex]];
+
+  return true;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+template<class T, class P>
+bool RandomItemDualIterate(const std::vector<T>& items, P test, T& returnItem)
+{
+  uint32_t matchingCount = 0;
+  for (const T& i : items)
+  {
+    // If matching the test
+    if (test(i))
+    {
+      matchingCount++;
+    }
+  }
+
+  if (matchingCount == 0)
+  {
+    return false;
+  }
+
+  // Select a random index
+  uint32_t matchIndex = RandRange(matchingCount - 1);
+  matchingCount = 0;
+  for (const T& i : items)
+  {
+    // If matching the test
+    if (test(i))
+    {
+      // If matching the index
+      if (matchingCount == matchIndex)
+      {
+        returnItem = i;
+        break;
+      }
+      matchingCount++;
+    }
+  }
+
+  return true;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+template<class T, class P>
+bool RandomItemSelect(const std::vector<T>& items, P test, T& returnItem)
 {
   uint32_t matchingCount = 0;
   for (const T& i : items)
@@ -84,14 +181,29 @@ int main()
 {
   s_csvFile.open("test_profile.csv");
 
-  s_csvFile.close();
+  // Create a test array with random data
+  std::vector<std::string> testData;
+  for (uint32_t i = 0; i < 10000; i++)
+  {
+    std::string newString;
+    newString.append(1, 'a' + RandRange(25));
+    newString.append(1, 'a' + RandRange(25));
+    newString.append(1, 'a' + RandRange(25));
+    newString.append(1, 'a' + RandRange(25));
+    newString.append(1, 'a' + RandRange(25));
+    testData.push_back(newString);
+  }
 
+  // Select from the array with the different methods
+  std::string foundItem;
+  RandomItemSelect(testData, [](const std::string& i) { return i[0] == 'a'; }, foundItem);
+
+
+  RandomItemSortArray(testData, [](const std::string& i) { return i[0] == 'a'; }, foundItem);
+
+  s_csvFile.close();
   return 0;
 }
 
-// Create a test array with random data
-
-
-// Select from the array with the different methods
 
 
